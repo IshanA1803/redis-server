a2bc59045c605b77bfa143b19bbe568201dfd97d
Ishan Aryan <singhishan67@gmail.com>
2026-07-15 18:28:06 +0000

    Load persisted database during server startup
    The server now attempts to load the database from a dump file
    during startup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include "../include/RedisServer.h"
+#include "../include/RedisDatabase.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
     int port = 6379;
-    if (argc >= 2)
-        port = std::stoi(argv[1]);
+
+    if (argc >= 2)port = std::stoi(argv[1]);
+
+    if (RedisDatabase::getInstance().load("dump.my_rdb"))
+        std::cout << "Database Loaded From dump.my_rdb\n";
+    else
+        std::cout << "No dump found or load failed; Starting with an empty database.\n";
 
     RedisServer server(port);
     server.run();
