0597374be9ff6508b138996632f5bcc799941398
Ishan Aryan <singhishan67@gmail.com>
2026-07-15 19:41:03 +0000

    Add background thread for periodic database persistence
    Started a detached background thread that periodically writes the
    in-memory database to disk.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "../include/RedisServer.h"
 #include "../include/RedisDatabase.h"
 #include <iostream>
+#include <thread>
+#include <chrono>
 
 int main(int argc, char* argv[]) {
     int port = 6379;
@@ -13,6 +15,20 @@ int main(int argc, char* argv[]) {
         std::cout << "No dump found or load failed; Starting with an empty database.\n";
 
     RedisServer server(port);
+
+    std::thread persistenceThread([]() {
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::seconds(300));
+
+            if (!RedisDatabase::getInstance().dump("dump.my_rdb"))
+                std::cerr << "Error Dumping Database\n";
+            else
+                std::cout << "Database Dumped to dump.my_rdb\n";
+        }
+    });
+
+    persistenceThread.detach();
+
     server.run();
 
     return 0;
